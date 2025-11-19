2b27f794c6246eb13af0bc9b3011c40255f05929
csikoslili <csikoslili2007@gmail.com>
2025-11-19 09:29:32 +0100

    működik!!!

--- projekt.py
+++ projekt.py
@@ -1,9 +1,10 @@
 host=int(input("add meg a hosztok számát:"))
 alap=input("add meg a hálózatot:")
-
-while True:
+a=0
+while a==0:
     for i in range(1,32):
         if int(2**i/host) == 1:
             print(i)
-            break
+            a=1
+        
 
